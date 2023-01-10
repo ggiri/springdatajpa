ec359b02a95b1400d2a80edea2c2a5ce3499fd1a
Giridhar Gopal <gopalgiridhar123@gmail.com>
2023-01-10 20:55:04 +0530

    Customer data project initial commit

--- productdata/src/test/java/com/giri/springdata/product/ProductdataApplicationTests.java
+++ productdata/src/test/java/com/giri/springdata/product/ProductdataApplicationTests.java
@@ -1,66 +1,66 @@
 package com.giri.springdata.product;
 
-import com.giri.springdata.product.entities.Product;
-import com.giri.springdata.product.repos.ProductRepository;
+import java.util.Optional;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.Optional;
+import com.giri.springdata.product.entities.Product;
+import com.giri.springdata.product.repos.ProductRepository;
 
 @SpringBootTest
 class ProductdataApplicationTests {
 
-	@Autowired
-	ProductRepository repository;
+    @Autowired
+    ProductRepository repository;
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
-	@Test
-	void testCreate(){
-		Product product = new Product();
-		product.setId(1);
-		product.setName("Pixel7");
-		product.setDesc("Great camera");
-		product.setPrice(60000.00);
+    @Test
+    void testCreate() {
+        Product product = new Product();
+        product.setId(1);
+        product.setName("Pixel7");
+        product.setDesc("Great camera");
+        product.setPrice(60000.00);
 
-		repository.save(product);
-		
-	}
-	
-	@Test
-	void testRead(){
-		Optional<Product> product = repository.findById(1);
-		Assertions.assertNotNull(product);
-		Assertions.assertEquals(1, product.get().getId());
-		Assertions.assertEquals("Pixel7", product.get().getName());
-		Assertions.assertEquals(60000.00, product.get().getPrice());
+        repository.save(product);
 
-	}
+    }
 
-	@Test
-	void testUpdate(){
-		Product product = repository.findById(1).get();
-		product.setDesc("Best pixel till date");
-		repository.save(product);
+    @Test
+    void testRead() {
+        Optional<Product> product = repository.findById(1);
+        Assertions.assertNotNull(product);
+        Assertions.assertEquals(1, product.get().getId());
+        Assertions.assertEquals("Pixel7", product.get().getName());
+        Assertions.assertEquals(60000.00, product.get().getPrice());
 
-	}
+    }
 
-	@Test
-	void testDelete() {
-		if(repository.existsById(1)){
-			repository.deleteById(1);
-		}
+    @Test
+    void testUpdate() {
+        Product product = repository.findById(1).get();
+        product.setDesc("Best pixel till date");
+        repository.save(product);
 
-	}
+    }
 
-	@Test
-	void testCount(){
-		System.out.println("Count of products: "+ repository.count());
-	}
+    @Test
+    void testDelete() {
+        if (repository.existsById(1)) {
+            repository.deleteById(1);
+        }
+
+    }
+
+    @Test
+    void testCount() {
+        System.out.println("Count of products: " + repository.count());
+    }
 
 }
