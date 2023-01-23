bdac7252074dba930358fe4c3a3aca77cb137b5d
GIRIDHARGOPAL <gopalgiridhar123@gmail.com>
2023-01-23 17:33:58 +0530

    Springboot configure external db and CRUD examples demo

--- mongodemo/src/test/java/com/giri/mongodb/MongodemoApplicationTests.java
+++ mongodemo/src/test/java/com/giri/mongodb/MongodemoApplicationTests.java
@@ -1,6 +1,10 @@
 package com.giri.mongodb;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,6 +30,38 @@ class MongodemoApplicationTests {
 		product.setPrice(50000);
 		Product savedProduct = repository.save(product);
 		assertNotNull(savedProduct);
-		}
+	}
+	
+	@Test
+	void testFindAll() {
+		List<Product> products = repository.findAll();
+		assertEquals(1, products.size());
+		
+	}
+	
+	@Test
+	void testDelete() {
+		repository.deleteById("63ce72db4d6fb126c9a19add");
+		Optional<Product> product = repository.findById("63ce72db4d6fb126c9a19add");
+		assertEquals(Optional.empty(), product);
+	}
+	
+	@Test
+	void createProductForUpdate() {
+		Product product = new Product();
+		product.setName("HP");
+		product.setPrice(50000);
+		Product savedProduct = repository.save(product);
+		assertNotNull(savedProduct);
+	}
+	
+	@Test
+	void updateProduct() {
+		Product product = repository.findById("63ce76a020407c16f0fa34c5").get();
+		product.setPrice(60000);
+		Product updatedProduct = repository.save(product);
+		assertEquals(60000, updatedProduct.getPrice());
+		
+	}
 
 }
